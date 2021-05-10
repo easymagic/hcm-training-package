9ffb6d544d93e03dc61736284488649c9f9c1e0d
easymagic <nnamware@yahoo.com>
2021-05-10 20:59:43 +0100

    np

---
+++ app/Http/Controllers/ModuleApprovalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ModuleApproval;
+use Illuminate\Http\Request;
+
+class ModuleApprovalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ModuleApproval  $moduleApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModuleApproval $moduleApproval)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ModuleApproval  $moduleApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModuleApproval $moduleApproval)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ModuleApproval  $moduleApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModuleApproval $moduleApproval)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ModuleApproval  $moduleApproval
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModuleApproval $moduleApproval)
+    {
+        //
+    }
+}
