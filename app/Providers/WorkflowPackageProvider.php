72c15147747d8b506967854c5bfbdded47b371c6
easymagic <nnamware@yahoo.com>
2021-05-21 11:55:04 +0100

    np

--- app/Providers/WorkflowPackageProvider.php
+++ app/Providers/WorkflowPackageProvider.php
@@ -14,7 +14,19 @@ class WorkflowPackageProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //this is where publishes is defined
+
+    $this->publishes([
+
+        __DIR__ . '/../../database/migrations/2021_05_10_201339_create_module_approvals_table.php'=>base_path('database/migrations/2021_05_10_201339_create_module_approvals_table.php'),
+        __DIR__ . '/../../resources/views/module_approval'=>base_path('resources/views/module_approval'),
+        __DIR__ . '/../../routes/'=>base_path('routes/workflow-routes/'),
+        __DIR__ . '/../../app/Services/ModuleApprovalV2Service.php'=>base_path('app/Services/ModuleApprovalV2Service.php'),
+        __DIR__ . '/../../app/Models/ModuleApproval.php'=>base_path('app/Models/ModuleApproval.php')
+
+    ]);
+
+
     }
 
     /**
