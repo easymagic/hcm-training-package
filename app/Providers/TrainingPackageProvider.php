8d8ae4fb9a10f19ff57fce686a5322c34e75b889
easymagic <nnamware@yahoo.com>
2021-05-24 10:14:54 +0100

    np

--- app/Providers/TrainingPackageProvider.php
+++ app/Providers/TrainingPackageProvider.php
@@ -43,7 +43,8 @@ class TrainingPackageProvider extends ServiceProvider
 
             __DIR__ . '/../../routes/'=>base_path('routes/training-package-routes/'),
             __DIR__ . '/../../resources/views/response-message.blade.php'=>base_path('resources/views/response-message.blade.php'),
-            __DIR__ . '/../../resources/views/js_cdns/swal.blade.php'=>base_path('resources/views/js_cdns/swal.blade.php')
+            __DIR__ . '/../../resources/views/js_cdns/swal.blade.php'=>base_path('resources/views/js_cdns/swal.blade.php'),
+            __DIR__ . '/../../app/Console/Commands/TrainingMigrationCommand.php'=>base_path('app/Console/Commands/TrainingMigrationCommand.php')
 
 
         ]);
